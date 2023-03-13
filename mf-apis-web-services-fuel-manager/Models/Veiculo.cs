e4a8dfc7fe273a301e4748e6f453df467f7b8efc
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2023-03-13 19:41:00 -0300

    Criação e configuração dos modelos de Veículo e Consumo

---
+++ mf-apis-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+        /* Navegação Virtual */
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
+
+// Anotações --------------------------------------
+/* Relação Veículos x Consumo é 1:n */
+/* Navegação Virtual: Como é 1:n, nessa tabela de veículos não precisamos ter nenhuma propriedade de consumo, mas podemos ter uma navegação virtual.*/
+/* 1 veículo possui vários consumos e 1 consumo está associado a apenas 1 veículo */
